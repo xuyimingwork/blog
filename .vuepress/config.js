db99d74c16cca4ad80650577a4302890d928dcf1
徐一鸣#80941041 <80941041@yonghui.cn>
2020-05-07 15:01:08 +0800

    feat: update icon for consistent display

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -7,7 +7,11 @@ module.exports = {
     ['meta', { name: 'theme-color', content: '#fff' }],
     ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
     ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'default' }],
-    ['link', { rel: 'apple-touch-icon', href: '/icon-apple-touch.png' }],
+    ['link', { rel: 'apple-touch-icon', sizes: '58x58', href: '/android-touch-icon.png' }],
+    ['link', { rel: 'apple-touch-icon', sizes: '76x76', href: '/apple-touch-icon-76x76.png' }],
+    ['link', { rel: 'apple-touch-icon', sizes: '120x120', href: '/apple-touch-icon-120x120.png' }],
+    ['link', { rel: 'apple-touch-icon', sizes: '152x152', href: '/apple-touch-icon-152x152.png' }],
+    ['link', { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon-180x180.png' }],
   ],
   theme: '@vuepress/theme-blog',
   themeConfig: {
