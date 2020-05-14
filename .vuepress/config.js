2bc714724d475dfca32c3f87dd45d7e697395340
徐一鸣#80941041 <80941041@yonghui.cn>
2020-05-14 18:24:19 +0800

    feat: disabled pwa

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -3,20 +3,7 @@ module.exports = {
   description: '每有会意 便欣然忘食',
   head: [
     ['link', { rel: 'icon', href: '/icon.png' }],
-    ['link', { rel: 'manifest', href: '/manifest.webmanifest' }],
-    ['meta', { name: 'theme-color', content: '#fff' }],
-    ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
-    ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'default' }],
-    ['link', { rel: 'apple-touch-icon', sizes: '58x58', href: '/android-touch-icon.png' }],
-    ['link', { rel: 'apple-touch-icon', sizes: '76x76', href: '/apple-touch-icon-76x76.png' }],
-    ['link', { rel: 'apple-touch-icon', sizes: '120x120', href: '/apple-touch-icon-120x120.png' }],
-    ['link', { rel: 'apple-touch-icon', sizes: '152x152', href: '/apple-touch-icon-152x152.png' }],
-    ['link', { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon-180x180.png' }],
   ],
-  plugins: ['@vuepress/pwa', {
-    serviceWorker: true,
-    updatePopup: true
-  }],
   theme: '@vuepress/theme-blog',
   themeConfig: {
     dateFormat: 'YYYY-MM-DD',
@@ -50,7 +37,6 @@ module.exports = {
         },
       ]
     },
-    // pwa: true
   },
   evergreen: true,
   markdown: {
